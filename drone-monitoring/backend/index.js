d1a3607ad0986933b8336e5052a5c9835658a97a
aliazahr <aliciaach06@outlook.fr>
2026-03-14 13:23:48 -0400

    Implement route to send drone position

--- drone-monitoring/backend/index.js
+++ drone-monitoring/backend/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+const droneRoutes = require("./routes/droneRoutes");
+
+app.use("/api", droneRoutes);
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
